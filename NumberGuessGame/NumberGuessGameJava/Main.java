6f00770a458a2e9dbbfddfb7b31aea0f3e98b819
mohamad almardini <mohamad.almardini@informatik.hs-fulda.de>
2025-07-29 12:40:29 +0200

    begrentze zahl an versuche eingebaut

--- NumberGuessGame/NumberGuessGameJava/Main.java
+++ NumberGuessGame/NumberGuessGameJava/Main.java
@@ -4,31 +4,43 @@ import java.util.Scanner;
  class Main{
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        int geheimzahl = random.nextInt(100) + 1;
-        int versuche = 0;
-        int eingabe;
-        System.out.println("willkommen bei dem spiel Guess the number");
-        do { 
-        System.out.println("raten sie bitte eine nummer zwischen 1 und 100");
-        eingabe = scanner.nextInt();
-        versuche++;
-        if(eingabe < geheimzahl){
-            System.out.println("leider klein");
-
-        }else if(eingabe > geheimzahl){
-            System.out.println("leider zu gross");
-
-        }else{
-            System.out.println("richtig geraten! du hast die nummer in " + versuche + "versuche");
+        try (Scanner scanner = new Scanner(System.in)) {
+            Random random = new Random();
             
+            int geheimzahl = random.nextInt(100) + 1;
+            int versuche = 0;
+            int eingabe;
+            System.out.println("willkommen bei dem spiel Guess the number");
+            System.out.println("raten sie bitte eine nummer zwischen 1 und 100");
+            System.out.println("sie haben nur 6 versuchen");
+            do {
+              
+                eingabe = scanner.nextInt();
+                 versuche++;
+              
+                if(eingabe < geheimzahl){
+                    System.out.println("leider klein");
+                    
+                }else if(eingabe > geheimzahl){
+                    System.out.println("leider zu gross");
+                    
+                }else{
+                    System.out.println("richtig geraten! du hast die nummer in " + versuche + " versuchen geraten.");
+                    
+                }
+                
+                
+            } while (eingabe != geheimzahl && versuche < 6);
+
+             if( eingabe != geheimzahl){
+                    System.out.println("Sie haben leider verloren, die Richtig zahl war : "+ geheimzahl);
+                    
+                }
+            
+        
+        
         }
-            
-            
-        } while (eingabe != geheimzahl);
-        scanner.close();
+        
 
 
     }
