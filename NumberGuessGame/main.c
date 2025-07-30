160ce2ae7543c1dc1b2f5f97717889235a456745
mohamad almardini <mohamad.almardini@informatik.hs-fulda.de>
2025-07-30 09:20:09 +0200

    wiederholung option bei c

--- NumberGuessGame/main.c
+++ NumberGuessGame/main.c
@@ -3,15 +3,22 @@
 #include<time.h>
 
 int main(){
+    char nochmalSpielen ;
+    do
+    {
+        
+    
     printf("Welcome to the Guessing Game!\n");
 
     int geheimzahl, tipp, versuche = 0;
+    
 
     srand(time(NULL));
     geheimzahl = rand() % 100 + 1;
 
     printf("ich habe mir eine zahl zwischen 1 und 100 ausgedacht.\n");
-    printf("versuch diese zahl jetzt zu raten");
+    printf("versuch diese zahl jetzt zu raten\n");
+    printf("sie haben 6 versuchen, um die nummer zu raten\n");
 
     do{
         printf("gib deinen tipp eine :");
@@ -20,16 +27,40 @@ int main(){
 
         if(tipp < geheimzahl){
             printf("leider zu klein.\n");
-        } else if (tipp > geheimzahl)
-        {
+            printf("sie haben noch %d versuchen\n" ,6 - versuche);
+        } else if (tipp > geheimzahl){
             printf("leider zu groß.\n");
+            printf("sie haben noch %d versuchen\n", 6 - versuche);
         }else{
             printf("Richtig. du hast die zahl in %d versuche geraten.\n", versuche);
+        
         }
        
         
         
-    } while (tipp != geheimzahl);
+    } while (tipp != geheimzahl && versuche < 6 );
+    if (tipp != geheimzahl)
+    {
+        printf("sie haben leider verloren\n");
+      
+
+
+    }
+      printf("möchten sie noch mal spielen \n");
+        printf("Antworten sie bitte mit Y für ja und mit N für nein (y/N)\n");
+        scanf(" %c" , &nochmalSpielen );
+        
+} while (nochmalSpielen == 'y' || nochmalSpielen == 'Y');
+if (nochmalSpielen == 'n' || nochmalSpielen == 'N')
+{
+    printf("das spiel wird jetzt beendet\n");
+}
+
+
+
+
+
+    
 
     return 0;
         
